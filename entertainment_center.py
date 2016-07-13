fb70fd30033f15a37e24fc1afd6fd32fed83b450
Simon Otter <simonotter@gmail.com>
2016-07-13 13:58:46 +0100

    Correct indentation on media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,17 +5,20 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy Story",
 						"A story of a boy and his toys that come to life",
 						"http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",
-						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc",
+						"22 Nov, 1995")
 
 avatar = media.Movie("Avatar",
 					 "A marine on an alien planet",
 					 "http://www.impawards.com/2009/posters/avatar_xlg.jpg",
-					 "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+					 "https://www.youtube.com/watch?v=d1_JBMrrYw8",
+					 "17 Dec, 2009")
 
-school_of_rock = media.Movie("School of Rock",
-							 "Storyline",
-							 "https://s-media-cache-ak0.pinimg.com/236x/22/2c/a5/222ca550b58ffab0a7b2979835e8c6c8.jpg",
-							 "https://www.youtube.com/watch?v=LqEszt5wG9I")
+school_of_rock = media.Movie("The Dark Knight Rises",
+							 "Eight years after the Joker's reign of anarchy, the Dark Knight, with the help of the enigmatic Selina, is forced from his imposed exile to save Gotham City, now on the edge of total annihilation, from the brutal guerrilla terrorist Bane.",
+							 "http://ia.media-imdb.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+							 "https://www.youtube.com/watch?v=g8evyE9TuYk",
+							 "20 Jul, 2012")
 
 
 # Add all three movies to a list
